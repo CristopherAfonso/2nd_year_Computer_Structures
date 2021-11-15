6cf0521ead1764adcf82b00004a51addfb23f5c9
CristopherAfonso <alu0101402031@ull.edu.es>
2021-11-15 11:09:50 +0000

    fa simplificado

--- P01_Diseño_ALU/fa.v
+++ P01_Diseño_ALU/fa.v
@@ -3,8 +3,5 @@
 // Cristopher Manuel Afonso Mora - alu0101402031
 
 module fa(output wire cout, sum, input wire a, b, cin);
-  wire sum1, carry1, carry2;
-  assign {carry1, sum1} = a + b;
-  assign {carry2, sum} = cin + sum1;
-  assign {cout} = carry1 | carry2;
+  assign {cout, sum} = a + b + cin;
 endmodule
